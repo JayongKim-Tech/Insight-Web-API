629cdef12af360d836d0a341330a02b49dc3f78c
unknown <rlawkdyd13@gmail.com>
2026-08-03 15:17:05 +0900

    test

--- VisionCore/ViewModels/MainViewModel.cs
+++ VisionCore/ViewModels/MainViewModel.cs
@@ -26,6 +26,7 @@ public class MainViewModel : ViewModelBase
 
     public LoggerService logger => LoggerService.Instance;
 
+    //test
 
     public ICommand ConnectCommand { get; }
     public ICommand CloseCommand { get; }
